07288b842eaf23eb65e3cda0ce5beb7c34cb57b8
shot9269 <110fe603@gmail.com>
2020-12-03 23:06:37 +0900

    時間の表示

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   before_action :authenticate_user!, only: [:new,:create]
 
   def index
-    @blogs = Blog.where(genre_id: params[:genre_id])
+    @blogs = Blog.where(genre_id: params[:genre_id]).order(created_at: "DESC")
   end
 
   def new
