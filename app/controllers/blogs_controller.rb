cc72f1e5696e2189a5fd8201a0fb35527d172e58
shot9269 <110fe603@gmail.com>
2020-12-03 18:31:51 +0900

    viewの変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,7 +16,7 @@ class BlogsController < ApplicationController
 
   def show
     @blog = Blog.find(params[:id])
-    render json: {blog: @blog}
+    render json:{blog: @blog}
   end
 
   private
--- app/javascript/index.js
+++ app/javascript/index.js
@@ -2,19 +2,18 @@ function blog(){
   const blogs = document.querySelectorAll(".border");
   blogs.forEach(function(blog){
     blog.addEventListener("click",()=>{
-
       const blogId = blog.getAttribute("data-id")
       const xhr = new XMLHttpRequest();
+      
       xhr.open("GET",`blogs/${blogId}`, true);
       xhr.responseType = "json";
       xhr.send();
       xhr.onload = ()=>{
         const item = xhr.response.blog;
-        console.log(item)
         const detail = document.getElementById("blog-detail")
         detail.innerHTML = `
         <div class="blogItem" data-id=${item.id}>
-          <p>${item.content.body}</p>
+          ${item.content.body}
         </div>`;
       }
     });
